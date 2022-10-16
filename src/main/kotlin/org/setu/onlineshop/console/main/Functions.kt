c8341080b7b3fea972f0af222cc4dc4a317aedab
Emran Sabbagh <emran2156@gmail.com>
2022-10-16 10:55:24 +0100

    Adding and Removing functions

--- src/main/kotlin/org/setu/onlineshop/console/main/Functions.kt
+++ src/main/kotlin/org/setu/onlineshop/console/main/Functions.kt
@@ -4,5 +4,23 @@ import org.setu.onlineshop.console.models.ShopModel
 
 
 object Functions {
+    /**
+     * This object is where I store functions used for the CRUD functionality in the Kotlin app
+     */
 
+    @JvmStatic
+    fun addingProducts(array1 : ArrayList<ShopModel>, array2 : ArrayList<ShopModel>, index : Int){
+        /**
+         * This function adds product by index from array1 (products) to array2 (cart)
+         */
+        array2 += array1[index-1]
+    }
+
+
+    fun removeProducts(array : ArrayList<ShopModel>, index : Int){
+        /**
+         * This function removes products from array (cart) by index
+         */
+        array.removeAt(index-1)
+    }
 }
