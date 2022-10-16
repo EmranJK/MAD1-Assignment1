4b102070008e9cae9aa03c0f24a5a44cf4ccd6e5
Emran Sabbagh <emran2156@gmail.com>
2022-10-16 10:57:08 +0100

    Listing and Searching functions

--- src/main/kotlin/org/setu/onlineshop/console/main/Functions.kt
+++ src/main/kotlin/org/setu/onlineshop/console/main/Functions.kt
@@ -23,4 +23,34 @@ object Functions {
          */
         array.removeAt(index-1)
     }
+
+
+    fun listingCartOrProducts(array : ArrayList<ShopModel>) : String{
+        /**
+         * This function lists all products in an array
+         */
+
+        var temp = ""
+        for (i in 0..array.size-1){
+            temp+=(array[i].product_name + " " + "($" + array[i].price.toString() + ") " + array[i].id + " " + array[i].category + " (" + array[i].brand+")\n")
+        }
+        return temp
+    }
+
+
+
+    fun searchingProducts(array : ArrayList<ShopModel>, name: String) : String{
+        /**
+         * This function searches for a product by name in an array
+         * The search is not case-sensitive
+         * The user can also provide 1 or more chars that the product name starts with instead of typing the whole name
+         */
+        for (i in 0..array.size-1){
+            //if (array[i].product_name == name){
+            if (array[i].product_name.startsWith(name, ignoreCase = true)){
+                return ("FOUND IT: " + (i+1).toString() + ". " + array[i].product_name + " " + "( $ " + array[i].price.toString() + " )" + " " + array[i].id.toString() + " " + array[i].category + " " + array[i].brand)
+            }
+        }
+        return ("Didn't Find It")
+    }
 }
